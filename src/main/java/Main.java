464a47b8953c4c26a5ba851291bd52e8d5bccdbd
Caroline Rives <car.rives@gmail.com>
2022-06-22 14:32:59 -0400

    Loop Lab Assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,17 @@ public class Main {
         // simulate the passing of time
         int startingYear = 2000;
         int targetYear = 2011;
-        int currentYear = startingYear;
+        int currentYear = startingYear + 1;
         while (currentYear < targetYear) {
             System.out.println((currentYear - startingYear) + " year(s) have passed");
             // conditional logic based on the current year
             currentYear++;
         }
+
+        // Extension: (Uncomment this and comment out the while loop above to avoid variable overriding)
+        // for (int i = startingYear; i < targetYear; i++) {
+        //     System.out.println((currentYear - startingYear) + " year(s) have passed");
+        //     currentYear++;
+        // }
     }
 }
